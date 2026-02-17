a55d63572d5070264b0a5c6b66adae778e18524b
redbonky <redbonky@gmail.com>
2026-02-17 00:21:08 +0000

    e

--- orbitsim.py
+++ orbitsim.py
@@ -3,17 +3,14 @@ import time
 import keyboard
 import os
 import copy
-import msvcrt as key
+import msvcrt
+import ctypes
 gravityconst = 6.674e-11
-height = 49
-width = 118
 keyboard.send('F11')
-scale = 100
+scale = 64
+gamespeed = 1
 camy = 0
 camx = 0
-framerate = 0
-frames = 0
-time1 = time.time()
 colors = {"black":"\033[38;5;0m",
           "red":"\033[38;5;1m",
           "green":"\033[38;5;2m",
@@ -38,9 +35,85 @@ requestlist = {"camhelper":0,
                "particledist":5,
                "speedmeasure":6,
                "orbitdist":7}
+def input2(text=""):
+    while msvcrt.kbhit():
+        msvcrt.getch()
+    val = input(text)
+    return val
+def print2(*args):
+    print(*args,sep="",end="")
+    return
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
+def getWindow():
+    hwnd = ctypes.windll.user32.GetForegroundWindow()
+    length = ctypes.windll.user32.GetWindowTextLengthW(hwnd)
+    buff = ctypes.create_unicode_buffer(length + 1)
+    ctypes.windll.user32.GetWindowTextW(hwnd, buff, length + 1)
+    return (buff.value, hwnd)
+os.system("title Orbit Sim")
+print2('\033[?25l')
+try:
+    open("settings.txt","x")
+except FileExistsError:
+    pass
+filedata = open("settings.txt","r").readlines()
+if len(filedata) != 2:
+    filedata = ""
+    print2("\033[2J")
+    border = [209,56]
+    while True:
+        if keyboard.is_pressed("w"):
+            border[1] -= 1
+            print2("\033[2J")
+        if keyboard.is_pressed("s"):
+            border[1] += 1
+            print2("\033[2J")
+        if keyboard.is_pressed("a"):
+            border[0] -= 1
+            print2("\033[2J")
+        if keyboard.is_pressed("d"):
+            border[0] += 1
+            print2("\033[2J")
+        if keyboard.is_pressed("space"):
+            windowsize = [border[0],border[1]]
+            break
+        print2("\033[",round(border[1]/2),"H")
+        print2("\033[",round(border[0]/2),"GWindow height: ",border[1],"\n")
+        print2("\033[",round(border[0]/2),"GWindow width: ",border[0],"\n")
+        print2("\033[",round(border[0]/2),"GUse WASD to move borders until they're at the furthest corners\n")
+        print2("\033[",round(border[0]/2),"Gof your screen while staying fully visible and not glitching\n")
+        print2("\033[",round(border[0]/2),"GUse spacebar to confirm screen size\n")
+        print2("\033[",round(border[0]/2),"G(on the computers I expect this to be used on the width should be 209 and height 56)")
+        print2("\033[H+------\n")
+        for a in range(4):
+            print2("|\n")
+        print2("\033[",border[1]-5,"H")
+        for a in range(4):
+            print2("|\n")
+        print2("+------\n\033[H")
+        print2("\033[",border[0]-6,"G------+\n")
+        for a in range(4):
+            print2("\033[",border[0],"G|\n")
+        print2("\033[",border[1]-5,"H")
+        for a in range(4):
+            print2("\033[",border[0],"G|\n")
+        print2("\033[",border[0]-6,"G------+\n")
+        time.sleep(0.1)
+    filedata += str(windowsize[0])+"\n"
+    filedata += str(windowsize[1])+"\n"
+    with open("settings.txt","w") as file:
+        for row in filedata:
+            file.write(row)
+with open("settings.txt","r") as file:
+    content = file.readlines()
+    windowsize = (int(content[0].strip()),int(content[1].strip()))
+width = int(windowsize[0]/2)-1
+height = windowsize[1]-10
+os.system("cls")
 while True:
     try:
-        (particlecode,requestcode) = input("Input particle and data combination code:").split('; ')
+        (particlecode,requestcode) = input2("Input particle and data combination code:").split('; ')
         particlecode = particlecode.split(', ')
         particles = []
         #Y, X, Y Velocity, X Velocity, Mass, Color, Name
@@ -60,60 +133,60 @@ while True:
                     requests += [[requestlist[requestcode[i][0]],int(requestcode[i][1]),int(requestcode[i][2])]]
                 case 6:
                     requests += [[requestlist[requestcode[i][0]],int(requestcode[i][1]),0,1e100]]
-                case 7: #ID, Target 1, Target 2, 3-8 bullshit, Show Orbit, Orbit Color
+                case 7: #ID, Target 1, Target 2, 3-8 stuff, Show Orbit, Orbit Color
                     requests += [[requestlist[requestcode[i][0]],int(requestcode[i][1]),int(requestcode[i][2]),0,0,0,1e100,0,0,int(requestcode[i][3]),colors[requestcode[i][4].lower()]]]
     except:
-        print("Invalid code, read the included guide")
+        print2("\nInvalid code, read the included guide\n")
     else:
         break
 requestsorig = copy.deepcopy(requests)
-os.system('cls')
-print('\033[?25l',end='')
+os.system("cls")
 while True:
-    button = []
-    while key.kbhit():
-        button += [key.getch().decode("ASCII")]
-    for i in button:
-        match i:
-            case "q":
-                scale /= 10
-            case "e":
-                scale *= 10
-            case "w":
-                camy += 1
-            case "s":
-                camy -= 1
-            case "d":
-                camx += 1
-            case "a":
-                camx -= 1
-            case "r":
-                camx = 0
-                camy = 0
-            case "v":
-                scale = int(input("Input scale level"))
-            case "p":
-                requests = copy.deepcopy(requestsorig)
+    while getWindow()[0] != "Orbit Sim":
+        time.sleep(0.1)
+    if keyboard.is_pressed("q"):
+        scale /= 2
+    if keyboard.is_pressed("e"):
+        scale *= 2
+    if keyboard.is_pressed("z"):
+        gamespeed /= 2
+    if keyboard.is_pressed("c"):
+        gamespeed *= 2
+    if keyboard.is_pressed("w"):
+        camy += 1
+    if keyboard.is_pressed("s"):
+        camy -= 1
+    if keyboard.is_pressed("d"):
+        camx += 1
+    if keyboard.is_pressed("a"):
+        camx -= 1
+    if keyboard.is_pressed("r"):
+        camx = 0
+        camy = 0
+    if keyboard.is_pressed("p"):
+        requests = copy.deepcopy(requestsorig)
+    scale = clamp(scale,1,scale)
+    gamespeed = clamp(gamespeed,1,512)
     for i in range(height,0,-1):
         for j in range(width):
             placed = False
             for k in range(len(particles)):
                 if i == round(particles[k][0]/scale-camy+height/2) and j == round(particles[k][1]/scale-camx+width/2) and placed == False:
-                    print(particles[k][5],end='')
-                    print('.',end='')
-                    print(colors["white"],end=' ')
+                    print2(particles[k][5])
+                    print2('.')
+                    print2(colors["white"],' ')
                     placed = True
             for g in range(len(requests)):
                 if requests[g][0] == 7:
                     if requests[g][9] == 1 and placed == False:
                         if (i == round(requests[g][4]/scale-camy+height/2) and j == round(requests[g][5]/scale-camx+width/2)) or (i == round(requests[g][7]/scale-camy+height/2) and j == round(requests[g][8]/scale-camx+width/2)):
-                            print(requests[g][10],end='')
-                            print(',',end='')
-                            print(colors["white"],end=' ')
+                            print2(requests[g][10])
+                            print2(',')
+                            print2(colors["white"],' ')
                             placed = True
             if placed == False:
-                print(' ',end=' ')
-        print('')
+                print2('  ')
+        print2('\n')
     for k in range(len(particles)):
         for b in range(len(particles)):
             if k != b:
@@ -130,53 +203,53 @@ while True:
                 #Apply direction of gravity based on position
                 if particles[k][0] > particles[b][0]:
                     #particle K is above particle B
-                    particles[k][2] -= ypart
+                    particles[k][2] -= ypart/20*gamespeed
                 elif particles[k][0] < particles[b][0]:
                     #particle K is below particle B
-                    particles[k][2] += ypart
+                    particles[k][2] += ypart/20*gamespeed
                 if particles[k][1] > particles[b][1]:
                     #particle K is to the right of particle B
-                    particles[k][3] -= xpart
+                    particles[k][3] -= xpart/20*gamespeed
                 elif particles[k][1] < particles[b][1]:
                     #particle K is to the left of particle B
-                    particles[k][3] += xpart
+                    particles[k][3] += xpart/20*gamespeed
     for k in range(len(particles)):
-        particles[k][0] += particles[k][2]
-        particles[k][1] += particles[k][3]
+        particles[k][0] += particles[k][2]/20*gamespeed
+        particles[k][1] += particles[k][3]/20*gamespeed
     for i in range(len(requests)):
         k = requests[i][1]
         match requests[i][0]:
             case 0:
-                print(f"Camera vector:({camx},{camy}) scale:{int(scale)}",end='')
+                print2(f"Camera vector:({camx},{camy}) scale:{int(scale)} speed:{int(gamespeed)}")
                 for l in range(len(particles)):
-                    print(f", {particles[l][6]} vector:({round(particles[l][1]/scale-camx)},{round(particles[l][0]/scale-camy)})",end='')
-                print("\033[K")
+                    print2(f", {particles[l][6]} vector:({round(particles[l][1]/scale-camx)},{round(particles[l][0]/scale-camy)})")
+                print2("\033[K\n")
             case 1: #Target 1
-                print(f"{particles[k][6]} position:({particles[k][1]:.2e},{particles[k][0]:.2e}) \033[K")
+                print2(f"{particles[k][6]} position:({particles[k][1]:.2e},{particles[k][0]:.2e}) \033[K\n")
             case 2: #Target 1
                 speed = (particles[k][2]**2+particles[k][3]**2)**0.5
-                print(f"{particles[k][6]} speed per tick:{speed:.2e}m \033[K")
+                print2(f"{particles[k][6]} speed:{speed:.2e}m/s \033[K\n")
             case 3: #Target 1, Target 2
                 b = requests[i][2]
                 particledist = ((particles[k][0]-particles[b][0])**2+(particles[k][1]-particles[b][1])**2)**0.5
                 tempgrav = (gravityconst*(particles[k][4]*particles[b][4])/(particledist**2))
-                print(f"Shared gravitational force between {particles[k][6]} and {particles[b][6]}:{tempgrav:.2e}N \033[K")
+                print2(f"Shared gravitational force between {particles[k][6]} and {particles[b][6]}:{tempgrav:.2e}N \033[K\n")
             case 4: #Target 1, Target 2
                 b = requests[i][2]
                 particledist = ((particles[k][0]-particles[b][0])**2+(particles[k][1]-particles[b][1])**2)**0.5
                 tempgrav = (gravityconst*(particles[k][4]*particles[b][4])/(particledist**2))/particles[k][4]
-                print(f"{particles[k][6]} gravitational force with {particles[b][6]}, adjusted for {particles[k][6]}'s mass:{tempgrav:.2e}N \033[K")
+                print2(f"{particles[k][6]} gravitational force with {particles[b][6]}, adjusted for {particles[k][6]}'s mass:{tempgrav:.2e}N \033[K\n")
             case 5: #Target 1, Target 2
                 b = requests[i][2]
                 particledist = ((particles[k][0]-particles[b][0])**2+(particles[k][1]-particles[b][1])**2)**0.5
-                print(f"Distance between {particles[k][6]} and {particles[b][6]}:{particledist:.2e}m \033[K")
+                print2(f"Distance between {particles[k][6]} and {particles[b][6]}:{particledist:.2e}m \033[K\n")
             case 6: #Target 1, Max Speed, Min Speed
                 speed = (particles[k][2]**2+particles[k][3]**2)**0.5
                 if speed > requests[i][2]:
                     requests[i][2] = speed
                 if speed < requests[i][3]:
                     requests[i][3] = speed
-                print(f"{particles[k][6]} Minimum speed per tick:{requests[i][3]:.2e}m Maximum speed per tick:{requests[i][2]:.2e}m \033[K")
+                print2(f"{particles[k][6]} Minimum speed:{requests[i][3]:.2e}m/s Maximum speed:{requests[i][2]:.2e}m/s \033[K\n")
             case 7: #Target 1, Target 2, Apoapsis dist, y, x, Periapsis dist, y, x, show orbit, periapsis and apoapsis color
                 b = requests[i][2]
                 particledist = ((particles[k][0]-particles[b][0])**2+(particles[k][1]-particles[b][1])**2)**0.5
@@ -188,13 +261,8 @@ while True:
                     requests[i][6] = particledist
                     requests[i][7] = particles[k][0]
                     requests[i][8] = particles[k][1]
-                print(f"Of {particles[k][6]} relative to {particles[b][6]}, Est. Apoapsis:({requests[i][5]:.2e},{requests[i][4]:.2e}),{requests[i][3]:.2e}m Est. Periapsis:({requests[i][8]:.2e},{requests[i][8]:.2e}),{requests[i][6]:.2e}m \033[K")
-                #print(f"{requests[i][3]+requests[i][6]:.2e},{((requests[i][5]-requests[i][7])**2+(requests[i][4]-requests[i][8])**2)**0.5:.2e}")
-    frames += 1
-    if frames == 10:
-        framerate = int(1/((time.time()-time1)/10))
-        time1 = time.time()
-        frames = 0
-    print(f"FPS: {framerate} \033[K")
-    print("\033[J")
-    print('\033[100A')
+                print2(f"Of {particles[k][6]} relative to {particles[b][6]}, Est. Apoapsis:({requests[i][5]:.2e},{requests[i][4]:.2e}),{requests[i][3]:.2e}m Est. Periapsis:({requests[i][8]:.2e},{requests[i][8]:.2e}),{requests[i][6]:.2e}m \033[K\n")
+                #print2(f"{requests[i][3]+requests[i][6]:.2e},{((requests[i][5]-requests[i][7])**2+(requests[i][4]-requests[i][8])**2)**0.5:.2e}")
+    time.sleep(0.05)
+    print2("\033[J")
+    print2("\033[100A")
